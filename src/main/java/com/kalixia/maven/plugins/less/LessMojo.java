5367c3810a064ea48919b7dd7350b637abaafbc9
Jerome Bernard <jerome.bernard@kalixia.com>
2011-09-23 21:06:28 +0200

    Compile LESS files in the WAR layout directory.

--- src/main/java/com/kalixia/maven/plugins/less/LessMojo.java
+++ src/main/java/com/kalixia/maven/plugins/less/LessMojo.java
@@ -2,10 +2,14 @@ package com.kalixia.maven.plugins.less;
 
 import com.asual.lesscss.LessEngine;
 import com.asual.lesscss.LessException;
+import org.apache.maven.model.PatternSet;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -14,24 +18,59 @@ import java.util.List;
  */
 public class LessMojo extends AbstractMojo {
     /**
+     * A set of file patterns to include LESS files
+     *
      * @parameter
      */
-    private List<File> includes;
+    private PatternSet includes;
 
     /**
-     * @parameter
+     * @parameter expression="${project.build.directory}/${project.build.finalName}"
      */
     private File outputDirectory;
 
+    /**
+     * @parameter expression="${basedir}/src/main/webapp"
+     */
+    private File webappSourceDirectory;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Searching for LESS files in " + webappSourceDirectory);
         LessEngine engine = new LessEngine();
-        for (File lessFile : includes) {
-            getLog().info("Compiling LESS file " + lessFile.getPath());
-            try {
-                engine.compile(lessFile);
-            } catch (LessException e) {
-                throw new MojoExecutionException("Could not compile LESS file " + lessFile.getPath(), e);
+        try {
+            @SuppressWarnings("unchecked")
+            List<File> lessFiles = FileUtils.getFiles(
+                    webappSourceDirectory,
+                    getCommaSeparatedList(includes.getIncludes()),
+                    getCommaSeparatedList(includes.getExcludes())
+            );
+            for (File lessFile : lessFiles) {
+                String cssFileLocation = lessFile.getPath().replace(".less", ".css").replace(webappSourceDirectory.getPath(), "");
+                File cssFile = new File(outputDirectory, cssFileLocation);
+                if (!cssFile.getParentFile().exists())
+                    cssFile.getParentFile().mkdirs();
+                getLog().info("Compiling LESS file " + lessFile.getPath() + " into " + cssFile.getPath());
+                try {
+                    engine.compile(lessFile, cssFile);
+                } catch (LessException e) {
+                    throw new MojoExecutionException("Could not compile LESS file " + lessFile.getPath(), e);
+                }
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not figure out which files to include/exclude", e);
+        }
+    }
+
+    protected String getCommaSeparatedList(List list) {
+        StringBuilder buffer = new StringBuilder();
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            Object object = it.next();
+            buffer.append(object.toString());
+            if (it.hasNext()) {
+                buffer.append(",");
             }
         }
+        return buffer.toString();
     }
 }
